f00e7ae5b9fb825f2e2856bb32d3752ea88ed311
harry247302 <shadab.shaikh.tech@gmail.com>
2025-06-10 18:36:52 +0530

    change in vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css'
 import 'react-toastify/dist/ReactToastify.css';
-
-
 import { Outlet } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react({
+    jsxRuntime: 'classic' // or 'automatic' depending on your needs
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
